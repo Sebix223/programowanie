ccef8f4e366361814dde57f8f3914cff7b1a9582
sebaros2333 <sebaros99@o2.pl>
2024-04-09 11:53:07 +0200

    wszystkie zadania

--- CiągGeometryczny.py
+++ CiągGeometryczny.py
@@ -1,2 +1,15 @@
-def CiagGometryczny(n,a1=1,q=2):
+from math import pow
+
+
+def a(n, a1=2, q=1):
+    if n == 1:
+        return a1
+    elif n == 2:
+        return a1 * q
+    else:
+        return a1 * pow(q, n - 1)
+
+
+def b(n, a1=1, q=2):
+    return a1 * (1 - pow(q, n)) / (1 - q)
 
--- lab3b.py
+++ lab3b.py
@@ -71,18 +71,9 @@
 
 # zad 6
 
-# from math import pow
-# def a(n, a1 = 1, q = 2):
-#     if n == 1:
-#         return a1
-#     elif n == 2:
-#         return a1 * q
-#     else:
-#         return a1 * pow(q, n-1)
-#
-# def b(n, a1 = 1, q = 2):
-#         return a1 * (1 - pow(q, n)) / (1 - q)
-#
+
+
+
 # from CiagGeometryczny import a, b
 #
 # print("an wyraz:", a(7, 3, 2))
